326365f27a47eab8a3c2edd3cf6e9193ec352e0e
trantri555 <23130347@st.hcmuaf.edu.vn>
2025-09-26 08:57:28 +0700

    Add files via upload
    bt ttnt

---
+++ 23130347_TranDucTri/23130347_TranDucTri_Tuan1/src/main/java/BT/Node.java
@@ -0,0 +1,24 @@
+package BT;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+	int state;
+	boolean visited;
+	List<Node> neighbours;
+	Node parent;
+
+	Node(int state) {
+		this.state = state;
+		this.neighbours = new ArrayList<>();
+		this.parent = null;
+	}
+	public void addNeighbours(Node neighbourNode) {
+		this.neighbours.add(neighbourNode);
+	}
+	public List getNeighbour() {
+		return neighbours;
+	}
+}
+
